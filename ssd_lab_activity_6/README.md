5079256ee9a9dfe684b9f9dec86b361067bf19b8
priyanshu988 <priyanshubansal988@gmail.com>
2022-09-15 17:10:41 +0530

    ACTIVITY 6

---
+++ ssd_lab_activity_6/README.md
@@ -0,0 +1,18 @@
+# Lab Activity 6
+
+## HTML files
+- index.html
+    - Created a Team Form page using form element in html. Webpage created is not responsive to vertical resizing. Every content in the page is enclosed within a common div. In this there is a drag drop box and a dropdown menu. In addition, It validates form fields as well.
+    - Toggle in dark mode is done by pressing ctrl +M.
+    
+
+## CSS file
+- mystyles.css
+    - Used CSS to design the page and make the page more user friendly
+
+## JS file
+- myscripts.js
+    - In JS file, I created function for drag and drop, and a alert funtion which is printing the value filled in form.
+    - also it has function which toggles the mode into dark/light by keypress.
+### Note
+- All the files should be present in the same folder before running the code.
---
+++ ssd_lab_activity_6/myscripts.js
@@ -0,0 +1,70 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function myKeyPress(e) {
+    var keynum;
+
+    if (window.event) { // IE                  
+        keynum = e.keyCode;
+    } else if (e.which) { // Netscape/Firefox/Opera                 
+        keynum = e.which;
+    }
+
+
+}
+function alertfunction() {
+
+    // validation code here ...
+
+    var cell1 = document.getElementById("name").value;
+
+    var cell2 = document.getElementById("email").value;
+
+    var cell3 = document.getElementById("username").value;
+
+    var cell4 = document.getElementById("lead").value;
+    alert("Manager Name: " + cell1 + "\n" + "Group Email: " + cell2 + "\n" + "Server Username: " + cell3 + "\n" + "Team lead: " + cell4);
+
+}
+// window.addEventListener('keydown', function (e) {
+    
+//     if (e.key = "control" )
+//     {this.onclick=myFunction();}
+//   }, false);
+
+
+  function KeyPress(e) {
+    var evtobj = window.event? event : e
+    if (evtobj.keyCode == 77 && evtobj.ctrlKey) this.onclick=myFunction(); ;
+}document.onkeydown = KeyPress;
+
+function passwordcheck()
+{
+    let in1=document.getElementById("password").value;
+    let in2=document.getElementById("cpassword").value;
+    
+    if(in1==in2)
+    {
+        return 1;
+    }
+    else
+    {
+        alert("Password Not Matched");
+        return 0;
+    }
+}
